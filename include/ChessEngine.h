903800b3a58c8fcd3aecf660c9fd1870f39b5357
leon <leon_albz@hotmail.com>
2026-03-11 21:44:08 +0000

    perf: Added __restrict to all pointers to arrays

--- include/ChessEngine.h
+++ include/ChessEngine.h
@@ -74,125 +74,10 @@
 
 
 class ChessEngine {
-public:
-	
-	ChessEngine(int depth, unsigned int seed, int engineColour);
-
-	/*
-	* Load position from fen
-	*/
-	void loadFromFEN(std::string FEN);
-
-	/*
-	*
-	*/
-	void initZobristHashArray();
-
-	/*
-	* Print board state
-	*/
-	void printBoard();
-
-
-	/*
-	* Generate possible pseudo legal moves for selected piece
-	* POSSIBLE OPTIMISATION (plus readability improvements), change nested ifs to multiple conditions
-	*/
-	int generatePseudoLegalMoves(int* pseudoLegalMoves, int* piecePositions, int* board, int* lookup, char castlingRights, int enpassantSquare, int colourToPlay, int blackKingSquare, int whiteKingSquare);
-
-	inline void generatePawnWhite(int* pseudoLegalMoves, int &index, int sourceSquare, int colourToPlay, int enpassantSquare, int* board, int* lookup);
-	inline void generatePawnBlack(int* pseudoLegalMoves, int &index, int sourceSquare, int colourToPlay, int enpassantSquare, int* board, int* lookup);
-	inline void generateKnight(int* pseudoLegalMoves, int &index, int sourceSquare, int colourToPlay, int* board, int* lookup);
-	inline void generateDiagonal(int* pseudoLegalMoves, int &index, int max, int sourceSquare, int colourToPlay, int* board, int* lookup);
-	inline void generateSraight(int* pseudoLegalMoves, int &index, int max, int sourceSquare, int colourToPlay, int* board, int* lookup);
-	inline void generateKingWhite(int* pseudoLegalMoves, int &index, int sourceSquare, int* board, int* lookup, char castlingRights);
-	inline void generateKingBlack(int* pseudoLegalMoves, int &index, int sourceSquare, int* board, int* lookup, char castlingRights);
-
-	/*
-	* Play move made by player
-	* Return true if move iis legal, otherwise return false
-	*/
-	bool playPlayerMove(int encodedMove, int endcodedInfo);
-
-	int makeMove(int encodedMove, int encodedInfo, int* board, int* piecePositions, int* lookupPiece);
-	void unmakeMove(int encodedMove, int encodedInfo, int* board, int* piecePositions, int* lookupPiece);
-	bool verifyMove(int encodedMove, int encodedInfo, int* board, int* piecePositions, int* lookupPiece);
-	bool isKingInCheck(int* board, int kingSquare, int colourToPlay);
-	
-	
-	/// <summary>
-	/// Start perft function from outside caller
-	/// </summary>
-	void perft();
-
-	/// <summary>
-	/// Go through EVERY SINGLE POSITION reachable from current position and count stats like number of final positions reached, number of captures, enpassant etc.
-	/// For debugging purposes
-	/// </summary>
-	/// <param name="depthLeft"> Number of depths left to searcvh</param>
-	/// <param name="encodedInfo"> Info related to the board state</param>
-	/// <param name="board"> The actual board </param>
-	/// <param name="piecePositions"> The square occupied by ever piece on the board</param>
-	/// <param name="lookupPiece"> The index of each piece in the piecePositions array </param>
-	void perft(int depthLeft, int encodedInfo, int* board, int* piecePositions, int* lookupPiece);
-
-	/// <summary>
-	/// Getter function
-	/// </summary>
-	/// <returns> Return search cepth of engine</returns>
-	int getDepth();
-
-	/// <summary>
-	/// Start alpha beta pruning from outside caller and pick correct alpha beta pruning method based off of engine colour. Black = min, White = max
-	/// </summary>
-	/// <returns> Move played by engine</returns>
-	int playEngineMove();
-
-	/// <summary>
-	/// Alpha beta pruning MAX. Represents white
-	/// </summary>
-	/// <param name="alpha"> Alpha cutoff value </param>
-	/// <param name="beta"> Beta cutoff value </param>
-	/// <param name="depthLeft"> Number of depths left to search </param>
-	/// <param name="encodedInfo"> Info related to the board state</param>
-	/// <param name="board"> The actual board </param>
-	/// <param name="piecePositions"> The square occupied by ever piece on the board</param>
-	/// <param name="lookupPiece"> The index of each piece in the piecePositions array </param>
-	/// <returns> </returns>
-	int ABMax(int alpha, int beta, int depthLeft, int encodedInfo, int* board, int* piecePositions, int* lookupPiece);
-
-	/// <summary>
-	/// Alpha beta pruning MIN. Represents black
-	/// </summary>
-	/// <param name="alpha"> Alpha cutoff value </param>
-	/// <param name="beta"> Beta cutoff value </param>
-	/// <param name="depthLeft"> Number of depths left to search </param>
-	/// <param name="encodedInfo"> Info related to the board state</param>
-	/// <param name="board"> The actual board </param>
-	/// <param name="piecePositions"> The square occupied by ever piece on the board</param>
-	/// <param name="lookupPiece"> The index of each piece in the piecePositions array </param>
-	/// <returns> </returns>
-	int ABMin(int alpha, int beta, int depthLeft, int encodedInfo, int* board, int* piecePositions, int* lookupPiece);
-
-	/// <summary>
-	/// Evaluate the position reached
-	/// </summary>
-	/// <param name="encodedInfo"> Info related to the board state</param>
-	/// <param name="board"> The actual board </param>
-	/// <param name="piecePositions"> The square occupied by ever piece on the board</param>
-	/// <returns> Returns score of the position </returns>
-	int evaluate(int encodedInfo, int* board, int* piecePositions);
-
-	/*
-	* Move ordering
-	*/
-	void moveOrdering(int* pseudoLegalMoves, int moveNum);
-
-
 private:
 	// Engine colour
 	int m_engineColour;
-	
+
 	// Depth
 	int m_depth;
 
@@ -200,7 +85,7 @@ private:
 	int m_nextEncodedMove;
 
 	// Board state
-	int m_board[128];
+	__declspec(align(64)) int m_board[128];
 
 	// Piece locations
 	int m_piecePositions[33];
@@ -235,11 +120,125 @@ private:
 	// ZOBRIST HASING STUFF
 	// Zobrist hash array
 	int m_zobristHashArray[12 * 64 + 1 + 4 + 8];
-	
+
 	// Random number seed function
 	unsigned int m_randomSeed;
 
 	// Pseudo-random number generator
 	std::mt19937_64 m_PRNG_64;
+
+public:
+	
+	ChessEngine(int depth, unsigned int seed, int engineColour);
+
+	/*
+	* Load position from fen
+	*/
+	void loadFromFEN(std::string FEN);
+
+	/*
+	*
+	*/
+	void initZobristHashArray();
+
+	/*
+	* Print board state
+	*/
+	void printBoard();
+
+
+	/*
+	* Generate possible pseudo legal moves for selected piece
+	* POSSIBLE OPTIMISATION (plus readability improvements), change nested ifs to multiple conditions
+	*/
+	int generatePseudoLegalMoves(int* __restrict pseudoLegalMoves, int* __restrict piecePositions, int* __restrict board, int* __restrict lookup, char castlingRights, int enpassantSquare, int colourToPlay, int blackKingSquare, int whiteKingSquare);
+
+	inline void generatePawnWhite(int* __restrict pseudoLegalMoves, int &index, int sourceSquare, int colourToPlay, int enpassantSquare, int* board, int* lookup);
+	inline void generatePawnBlack(int* __restrict pseudoLegalMoves, int &index, int sourceSquare, int colourToPlay, int enpassantSquare, int* board, int* lookup);
+	inline void generateKnight(int* __restrict pseudoLegalMoves, int &index, int sourceSquare, int colourToPlay, int* board, int* lookup);
+	inline void generateDiagonal(int* __restrict pseudoLegalMoves, int &index, int max, int sourceSquare, int colourToPlay, int* board, int* lookup);
+	inline void generateSraight(int* __restrict pseudoLegalMoves, int &index, int max, int sourceSquare, int colourToPlay, int* board, int* lookup);
+	inline void generateKingWhite(int* __restrict pseudoLegalMoves, int &index, int sourceSquare, int* board, int* lookup, char castlingRights);
+	inline void generateKingBlack(int* __restrict pseudoLegalMoves, int &index, int sourceSquare, int* board, int* lookup, char castlingRights);
+
+	/*
+	* Play move made by player
+	* Return true if move iis legal, otherwise return false
+	*/
+	bool playPlayerMove(int encodedMove, int endcodedInfo);
+
+	int makeMove(int encodedMove, int encodedInfo, int* __restrict board, int* __restrict piecePositions, int* __restrict lookupPiece);
+	void unmakeMove(int encodedMove, int encodedInfo, int* __restrict board, int* __restrict piecePositions, int* __restrict lookupPiece);
+	bool verifyMove(int encodedMove, int encodedInfo, int* __restrict board, int* __restrict piecePositions, int* __restrict lookupPiece);
+	bool isKingInCheck(int* board, int kingSquare, int colourToPlay);
+	
+	
+	/// <summary>
+	/// Start perft function from outside caller
+	/// </summary>
+	void perft();
+
+	/// <summary>
+	/// Go through EVERY SINGLE POSITION reachable from current position and count stats like number of final positions reached, number of captures, enpassant etc.
+	/// For debugging purposes
+	/// </summary>
+	/// <param name="depthLeft"> Number of depths left to searcvh</param>
+	/// <param name="encodedInfo"> Info related to the board state</param>
+	/// <param name="board"> The actual board </param>
+	/// <param name="piecePositions"> The square occupied by ever piece on the board</param>
+	/// <param name="lookupPiece"> The index of each piece in the piecePositions array </param>
+	void perft(int depthLeft, int encodedInfo, int* __restrict board, int* __restrict piecePositions, int* __restrict lookupPiece);
+
+	/// <summary>
+	/// Getter function
+	/// </summary>
+	/// <returns> Return search cepth of engine</returns>
+	int getDepth();
+
+	/// <summary>
+	/// Start alpha beta pruning from outside caller and pick correct alpha beta pruning method based off of engine colour. Black = min, White = max
+	/// </summary>
+	/// <returns> Move played by engine</returns>
+	int playEngineMove();
+
+	/// <summary>
+	/// Alpha beta pruning MAX. Represents white
+	/// </summary>
+	/// <param name="alpha"> Alpha cutoff value </param>
+	/// <param name="beta"> Beta cutoff value </param>
+	/// <param name="depthLeft"> Number of depths left to search </param>
+	/// <param name="encodedInfo"> Info related to the board state</param>
+	/// <param name="board"> The actual board </param>
+	/// <param name="piecePositions"> The square occupied by ever piece on the board</param>
+	/// <param name="lookupPiece"> The index of each piece in the piecePositions array </param>
+	/// <returns> </returns>
+	int ABMax(int alpha, int beta, int depthLeft, int encodedInfo, int* __restrict board, int* __restrict piecePositions, int* __restrict lookupPiece);
+
+	/// <summary>
+	/// Alpha beta pruning MIN. Represents black
+	/// </summary>
+	/// <param name="alpha"> Alpha cutoff value </param>
+	/// <param name="beta"> Beta cutoff value </param>
+	/// <param name="depthLeft"> Number of depths left to search </param>
+	/// <param name="encodedInfo"> Info related to the board state</param>
+	/// <param name="board"> The actual board </param>
+	/// <param name="piecePositions"> The square occupied by ever piece on the board</param>
+	/// <param name="lookupPiece"> The index of each piece in the piecePositions array </param>
+	/// <returns> </returns>
+	int ABMin(int alpha, int beta, int depthLeft, int encodedInfo, int* __restrict board, int* __restrict piecePositions, int* __restrict lookupPiece);
+
+	/// <summary>
+	/// Evaluate the position reached
+	/// </summary>
+	/// <param name="encodedInfo"> Info related to the board state</param>
+	/// <param name="board"> The actual board </param>
+	/// <param name="piecePositions"> The square occupied by ever piece on the board</param>
+	/// <returns> Returns score of the position </returns>
+	int evaluate(int encodedInfo, int* board, int* piecePositions);
+
+	/*
+	* Move ordering
+	*/
+	void moveOrdering(int* __restrict pseudoLegalMoves, int moveNum);
 };
 
